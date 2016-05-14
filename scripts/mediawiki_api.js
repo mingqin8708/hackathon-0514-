8b5c6e5ac91290d483357d23946e85dc835553f1
mingqin <mingqin.liu@duitang.com>
2016-05-14 17:38:48 +0800

    feature: ..

--- scripts/mediawiki_api.js
+++ scripts/mediawiki_api.js
@@ -3,30 +3,40 @@
  * Created by mingqin on 16/5/14.
  */
 
-var $explore = $("#explore");
-$explore.click(function() {
+var list = ['seal', 'whale', 'Predation','Wheatstone bridge']
+var num = getRandomArbitrary(0, list.length)
+var title = list[num]
+var explore = $("#explore");
+explore.click(function() {
     var url = "https://en.wikipedia.org/w/api.php";
     $.ajax({
         type: "GET",
         url: url,
-        dataType: 'jsonp',
+        dataType: 'json',
         data: {
-            'action': 'query',
-            'titles': 'theory', //random
-            'prop': 'revisions',
-            'format': 'jsonp',
+            'action'  : 'query',
+            'titles'  : title, //random
+            'prop'    : 'revisions',
+            'format'  : 'json',
+            'rvprop'  : 'content',
         },
         success: function(r) {
-            raw = r['data'];
-            console.log(raw)
+            var pages = r['query']['pages']
+            var pageid = Object.keys(pages)[0]
+            var wikistr = pages[pageid]['revisions'][0]['*']
+            var html = wikistr.wiki2html();
+            //var html = wiky.process(wikistr)
+            $('#msg').html(html)
+            console.log(r)
         },
         fail: function(r) {
-            raw = r['data']
-            console.log(raw)
+            console.log(r)
         }
     }).done(function(r){
-        raw = r['data']
-        $explore.prop('disabled',false);
+        console.log(r)
     });
-    console.log('xxx')
 })
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
